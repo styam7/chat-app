9ed7fc13cf56e307d3b6461cba5eb8f185bcbeda
styam7 <styam.7@gmail.com>
2022-08-04 12:53:30 +0530

    fetch messages

--- client/src/components/SingleChat.js
+++ client/src/components/SingleChat.js
@@ -1,131 +1,161 @@
-import { Box, FormControl, IconButton, Input, Spinner, Text, useToast } from '@chakra-ui/react'
-import React, { useState } from 'react'
-import { ChatState } from '../context/ChatContext'
-import { ArrowBackIcon } from '@chakra-ui/icons'
-import { getSender, getSenderFull } from '../config/ChatLogics'
-import ProfileModal from '../components/miscellaneous/ProfileModal'
-import UpdateGroupChatModal from './miscellaneous/UpdateGroupChatModal'
-import axios from 'axios'
+import {
+  Box,
+  FormControl,
+  IconButton,
+  Input,
+  Spinner,
+  Text,
+  useToast,
+} from "@chakra-ui/react";
+import React, { useEffect, useState } from "react";
+import { ChatState } from "../context/ChatContext";
+import { ArrowBackIcon } from "@chakra-ui/icons";
+import { getSender, getSenderFull } from "../config/ChatLogics";
+import ProfileModal from "../components/miscellaneous/ProfileModal";
+import UpdateGroupChatModal from "./miscellaneous/UpdateGroupChatModal";
+import axios from "axios";
 
 const SingleChat = ({ fetchAgain, setFetchAgain }) => {
+  const [messages, setMessages] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [newMessage, setNewMessage] = useState();
 
-    const [messages, setMessages] = useState([])
-    const [loading, setLoading] = useState(false)
-    const [newMessage, setNewMessage] = useState()
+  const toast = useToast();
+  const { user, selectedChat, setSelectedChat } = ChatState();
 
-    const toast = useToast()
-    const { user, selectedChat, setSelectedChat } = ChatState()
+  const fetchMessages = async () => {
+    if (!selectedChat) return;
 
-    const fetchMessages = () => { }
+    try {
+      setLoading(true);
 
-    const sendMessage = async (e) => {
-        if(e.key === "Enter" && newMessage) {
-            try {
-                setNewMessage("")
-                const { data } = await axios.post('/message', {
-                    content: newMessage,
-                    chatId: selectedChat._id
-                })
-                console.log(data)
-                setMessages([...messages, data])
-            } catch (err) {
-                toast({
-                    title: "Error Occured!",
-                    description: "Failed to send the Message",
-                    status: "error",
-                    duration: 5000,
-                    isClosable: true,
-                    position: "bottom",
-                });
-            }
-        }
+      const { data } = axios.get(`/message/${selectedChat._id}`);
+      setMessages(data);
+      setLoading(false);
+    } catch (err) {
+      toast({
+        title: "Error Occured!",
+        description: "Failed to load Messages",
+        status: "error",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom",
+      });
+      setLoading(false);
     }
-    const typingHandler = async (e) => {
-        setNewMessage(e.target.value)
+  };
+
+  useEffect(() => {
+    fetchMessages();
+  }, [selectedChat]);
+
+  const sendMessage = async (e) => {
+    if (e.key === "Enter" && newMessage) {
+      try {
+        setNewMessage("");
+        const { data } = await axios.post("/message", {
+          content: newMessage,
+          chatId: selectedChat._id,
+        });
+        console.log(data);
+        setMessages([...messages, data]);
+      } catch (err) {
+        toast({
+          title: "Error Occured!",
+          description: "Failed to send the Message",
+          status: "error",
+          duration: 5000,
+          isClosable: true,
+          position: "bottom",
+        });
+      }
     }
-    return (
+  };
+  const typingHandler = async (e) => {
+    setNewMessage(e.target.value);
+  };
+  return (
+    <>
+      {selectedChat ? (
         <>
-            {
-                selectedChat ? (
-                    <>
-                        <Text
-                            fontSize={{ base: "28px", md: "30px" }}
-                            pb={3}
-                            px={2}
-                            w="100%"
-                            fontFamily="Work sans"
-                            display="flex"
-                            justifyContent={{ base: "space-between" }}
-                            alignItems="center"
-                        >
-                            <IconButton
-                                display={{ base: "flex", md: "none" }}
-                                icon={<ArrowBackIcon />}
-                                onClick={() => setSelectedChat("")}
-                            />
-                            {!selectedChat.isGroupChat ? (
-                                <>
-                                    {getSender(user, selectedChat.users)}
-                                    <ProfileModal user={getSenderFull(user, selectedChat.users)} />
-                                </>
-                            ) : (
-                                <>
-                                    {selectedChat.chatName.toUpperCase()}
-                                    <UpdateGroupChatModal
-                                        fetchMessages={fetchMessages}
-                                        fetchAgain={fetchAgain}
-                                        setFetchAgain={setFetchAgain}
-                                    />
-                                </>
-                            )}
-                        </Text>
-                        <Box
-                            display="flex"
-                            flexDir="column"
-                            justifyContent="flex-end"
-                            p={3}
-                            bg="#E8E8E8"
-                            w="100%"
-                            h="100%"
-                            borderRadius="lg"
-                            overflowY="hidden"
-                        >
-                            {loading ? (
-                                <Spinner
-                                    size="xl"
-                                    w={20}
-                                    h={20}
-                                    alignSelf="center"
-                                    margin="auto"
-                                />)
-                                :
-                                (
-                                    <>
-                                    </>
-                                )}
-                            <FormControl onKeyDown={sendMessage} isRequired mt={3}>
-                                <Input
-                                    variant="filled"
-                                    bg="#E0E0E0"
-                                    placeholder='Type here...'
-                                    onChange={typingHandler}
-                                    value={newMessage}
-                                />
-                            </FormControl>
-                        </Box>
-                    </>
-                )
-                    :
-                    (
-                        <Box display="flex" alignItems="center" justifyContent="center" h="100%">
-                            <Text fontSize="3xl" pb={3} fontFamily="Work sans">
-                                Click on a user to start chatting
-                            </Text>
-                        </Box>
-                    )
-            }
+          <Text
+            fontSize={{ base: "28px", md: "30px" }}
+            pb={3}
+            px={2}
+            w="100%"
+            fontFamily="Work sans"
+            display="flex"
+            justifyContent={{ base: "space-between" }}
+            alignItems="center"
+          >
+            <IconButton
+              display={{ base: "flex", md: "none" }}
+              icon={<ArrowBackIcon />}
+              onClick={() => setSelectedChat("")}
+            />
+            {!selectedChat.isGroupChat ? (
+              <>
+                {getSender(user, selectedChat.users)}
+                <ProfileModal user={getSenderFull(user, selectedChat.users)} />
+              </>
+            ) : (
+              <>
+                {selectedChat.chatName.toUpperCase()}
+                <UpdateGroupChatModal
+                  fetchMessages={fetchMessages}
+                  fetchAgain={fetchAgain}
+                  setFetchAgain={setFetchAgain}
+                />
+              </>
+            )}
+          </Text>
+          <Box
+            display="flex"
+            flexDir="column"
+            justifyContent="flex-end"
+            p={3}
+            bg="#E8E8E8"
+            w="100%"
+            h="100%"
+            borderRadius="lg"
+            overflowY="hidden"
+          >
+            {loading ? (
+              <Spinner
+                size="xl"
+                w={20}
+                h={20}
+                alignSelf="center"
+                margin="auto"
+              />
+            ) : (
+              <></>
+            )}
+            <FormControl onKeyDown={sendMessage} isRequired mt={3}>
+              <Input
+                variant="filled"
+                bg="#E0E0E0"
+                placeholder="Type here..."
+                onChange={typingHandler}
+                value={newMessage}
+              />
+            </FormControl>
+          </Box>
         </>
-    )
-}
+      ) : (
+        <Box
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+          h="100%"
+        >
+          <Text fontSize="3xl" pb={3} fontFamily="Work sans">
+            Click on a user to start chatting
+          </Text>
+        </Box>
+      )}
+    </>
+  );
+};
 
-export default SingleChat
+export default SingleChat;
